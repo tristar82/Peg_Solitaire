aaaf950392f13a1548c5a5e69f9b35be4f6715c3
Elliott Coyne <56124104+tristar82@users.noreply.github.com>
2021-11-09 21:35:21 +0000

    updated functions to add comments and more error handling for imported files.

--- GUI_main.py
+++ GUI_main.py
@@ -17,86 +17,145 @@ peg = Peg()
 board_ = Board()
 
 global pegs_on_board
-
-
-game_started = False
-pegs_on_board = 32
-peg_pos_dict = board_.peg_pos_dict
-####### end of import from Text Game
-
-global state_of_play  # can remove if commenting out the bottom line below.
+global state_of_play
 global org_coords_input
 global dest_coords_input
 
-
+peg_pos_dict = board_.peg_pos_dict
+game_started = False
+pegs_on_board = 32
 state_of_play = 0
 org_coords_input = None
 dest_coords_input = None
 
-
 class Application(tk.Frame):
     def __init__(self, master):
-        """This initializes the frame"""
+        """This initializes the application frame"""
         super(Application, self).__init__(master)
         self.grid()
         self.coords = []
         self.chars_copy = None
         self.base = [chr(i) for i in range(97, 113)]
         self.chars = self.base + ['x'] + [i.upper() for i in self.base[::-1]]
-
         self.create_grid_coords()
         self.create_widgets()
-
-        #self.buttons()
-
-
+        self.board = board_.board
+        self.user_org_coords = [9, 9]  # using for move validation when importing files
+        self.user_dest_coords = [9, 9]  # using for move validation when importing files
 
     def display_label(self):
-        tk.Label(text="{} Pegs Remaining".format(pegs_on_board)).grid(row=7, column=0)
-        #tk.Label(text="TMP Awaiting Next Instruction".format(pegs_on_board)).grid(row=8, column=0)
+        '''Function to display number of pegs remaining'''
+        tk.Label(text="{}".format(pegs_on_board)).grid(row=7, column=0)
 
     def create_grid_coords(self):
+        '''Function to create board grid coordinates for GUI'''
         raw_grid = [[not row == ele == 3 if ele in [2, 3, 4] or row in [2, 3, 4] else None for row in range(7)] for ele
                     in range(7)]
         for row in range(len(raw_grid)):
             for col in range(len(raw_grid[row])):
                 if raw_grid[row][col] is not None:
                     self.coords.append([row, col])
-        print(raw_grid)
-
 
     def quit_game(self):
+        '''
+        Function to save legal moves before quitting.
+        Calls on the existing file save function (which uses datetime for unique file names)
+        '''
         peg.auto_export_to_file()
         quit()
 
     def save_game(self):
+        '''
+        Function to save legal moves; calls on the existing
+        file save function (which uses datetime for unique file names)
+        '''
         peg.auto_export_to_file()
 
-    def load_moves(self):
+    # def load_moves(self):
+    #     '''Function to open text file, via file browser. Text file loaded into a variable'''
+    #     global pegs_on_board
+    #     self.fname = askopenfilename(filetypes=(("Solution Files", "*.txt"),("All files", "*.*") ))
+    #     if self.fname != '':
+    #         file_contents = open(self.fname)
+    #         data = file_contents.read()
+    #         file_contents.close()
+    #         user_loaded_moves = data.strip().split(',')
+    #
+    #         # Cycle though the loaded moves
+    #         for user_loaded_move in user_loaded_moves:
+    #             if len(user_loaded_move) == 2:
+    #
+    #             # Extract the origin peg
+    #             user_org_coords = peg_pos_dict[user_loaded_move[0]]
+    #             # Extract the destination peg
+    #             user_dest_coords = peg_pos_dict[user_loaded_move[1]]
+    #
+    #             # Update the board as per the legal move
+    #             peg.update_board_pegs(user_org_coords, \
+    #                                   peg.validated_middle_peg(user_org_coords, user_dest_coords)[1],
+    #                                   user_dest_coords, board_.board)
+    #             peg.add_move(user_loaded_move[0], user_loaded_move[1])
+    #
+    #             pegs_on_board -= 1
+    #             self.create_widgets()
+
+    def reset_game(self):
+        '''
+        Function to reset game
+        '''
+        self.board = board_.board
+        pegs_on_board = 32
+        game_started = False
+        state_of_play = 0
+        self.create_widgets()
+        print("reset game")
+
+
+    def load_moves2(self):
         '''Function to open text file, via file browser. Text file loaded into a variable'''
         global pegs_on_board
-        self.fname = askopenfilename(filetypes=(("Soltuion Files", "*.txt"),("All files", "*.*") ))
-        file_contents = open(self.fname)
-        data = file_contents.read()
-        file_contents.close()
-        user_loaded_moves = data.strip().split(',')
-        # Cycle though the loaded moves
-        for user_loaded_move in user_loaded_moves:
-            # Extract the origin peg
-            user_org_coords = peg_pos_dict[user_loaded_move[0]]
-            # Extract the destination peg
-            user_dest_coords = peg_pos_dict[user_loaded_move[1]]
+        self.fname = askopenfilename(filetypes=(("Solution Files", "*.txt"), ("All files", "*.*")))
+        if self.fname != '':
+            file_contents = open(self.fname)
+            data = file_contents.read()
+            file_contents.close()
+            user_loaded_moves = data.strip().split(',')
 
-            # Update the board as per the legal move
-            peg.update_board_pegs(user_org_coords, \
-                                  peg.validated_middle_peg(user_org_coords, user_dest_coords)[1],
-                                  user_dest_coords, board_.board)
-            peg.add_move(user_loaded_move[0], user_loaded_move[1])
+            # Cycle though the loaded moves
+            for user_loaded_move in user_loaded_moves:
+                if len(user_loaded_move) == 2:
+                    # Extracting origin
+                    try:
+                        self.user_org_coords = peg_pos_dict[user_loaded_move[0]]
+                        # Extracting desintation
+                        self.user_dest_coords = peg_pos_dict[user_loaded_move[1]]
 
-            pegs_on_board -= 1
-            self.create_widgets()
+                        # Ensuring origin and middle are filled and destination are empty.
+                        move_direction = peg.validated_middle_peg(self.user_org_coords, self.user_dest_coords)[1]
+
+                        if board_.is_origin_filled(self.user_org_coords, board_.board) \
+                                and board_.is_destination_empty(self.user_dest_coords, board_.board) \
+                                and board_.is_middle_filled(self.user_org_coords, board_.board, move_direction):
+
+                            # Need to validate each loaded move
+                            # Origin and Destination characters must be peg position dictionary
+                            if peg.validated_middle_peg(self.user_org_coords, self.user_dest_coords)[0]:
+                                # Update the board as per the legal move
+                                peg.update_board_pegs(self.user_org_coords,
+                                                      peg.validated_middle_peg(self.user_org_coords, self.user_dest_coords)[1],
+                                                      self.user_dest_coords, board_.board)
+
+                                # Appending legal move to list
+                                peg.add_move(user_loaded_move[0], user_loaded_move[1])
+
+                                # Updating pegs on board up to reflect legal move
+                                pegs_on_board -= 1
+                                self.create_widgets()
+                    except:
+                        pass # if the move doesn't work, I want it to move to the next one.
 
     def create_widgets(self):
+        '''Function to create all colour coded buttons (i.e. pegs/ holes)'''
         self.chars_copy = self.chars.copy()
         for c in self.coords:
             button_letter = self.chars_copy.pop(0)
@@ -113,51 +172,50 @@ class Application(tk.Frame):
                       bg = button_colour,
                       command=lambda button_entry_coords = c : set_coords(button_entry_coords, state_of_play)
                       ).grid(row=c[0], column=c[1])
-        tk.Label(text="{} Pegs Remaining".format(pegs_on_board)).grid(row=7, column=0)
-        tk.Button(self, text="Quit", command=self.quit_game).grid(row=0, column=0)
-        tk.Button(self, text="Save", command=peg.auto_export_to_file).grid(row=1, column=0)
-        tk.Button(self, text="Load", command=self.load_moves).grid(row=7, column=0)
+
+        tk.Button(self, text="Load", command=self.load_moves2).grid(row=7, column=2)
+        tk.Button(self, text="Save", command=peg.auto_export_to_file).grid(row=7, column=3)
+        tk.Button(self, text="Reset", command=self.reset_game).grid(row=7, column=5)
+        tk.Button(self, text="Quit", command=self.quit_game).grid(row=7, column=4)
+
+        tk.Label(text=pegs_on_board).grid(row=7, column=0)
 
 
 
 def set_coords(coords, state_of_play_input):
     '''Function to capture the coords and their identity i.e. origin or destination'''
-    # global input_coords
     global org_coords_input
     global dest_coords_input
     global state_of_play  # can remove if commenting out the bottom line below.
 
-    tk.Label(text="{} Pegs Remaining".format(pegs_on_board)).grid(row=7, column=0)
+    tk.Label(text=pegs_on_board).grid(row=7, column=0)
 
     if state_of_play_input == 0:
         tk.Label(text="Click on Destination Hole").grid(row=8, column=0)
         org_coords_input = coords
         state_of_play = 1
-        #print("The origin has been set to {} and the state of play is {}".format(org_coords_input,state_of_play ))
+
     elif state_of_play_input == 1:
         dest_coords_input = coords
-        mega_function(org_coords_input, dest_coords_input)
+        game_play(org_coords_input, dest_coords_input)
         tk.Label(text="Click on Origin Peg").grid(row=8, column=0)
-        #print("The destination has been set to {} and the state of play is {}".format(dest_coords_input, state_of_play))
-        #state_of_play = 0
 
 
-########################################### START OF RECYCLED CODE
-def mega_function(org_coords, dest_coords):
+def game_play(org_coords, dest_coords):
     '''The purpose of this function is to call all of the functions created for the text game to:
     1 assess if a legal move has been put forward
     2 determine the direction of travel
     3 update the board if everything is in order'''
     global state_of_play
     global pegs_on_board
-    global  peg_pos_dict
+    global peg_pos_dict
 
     org_char = list(peg_pos_dict.keys())[list(peg_pos_dict.values()).index(org_coords)]
     dest_char = list(peg_pos_dict.keys())[list(peg_pos_dict.values()).index(dest_coords)]
 
     if not peg.validated_middle_peg(org_coords, dest_coords)[0]:
-        state_of_play = 0 #
-        print("Not a valid move. Select your next origin peg")
+        state_of_play = 0
+        #print("Not a valid move. Select your next origin peg")
     elif peg.validated_middle_peg(org_coords, dest_coords)[0]:
         ## Get direction of travel (origin to destination)
         move_direction = peg.validated_middle_peg(org_coords, dest_coords)[1]
@@ -177,10 +235,6 @@ def mega_function(org_coords, dest_coords):
                 peg.auto_export_to_file()
         else:
             print("Are ORIGIN and MIDDLE holes empty filled and DESTINATION empty?")
-
-########################################### END OF RECYCLED CODE
-
-
 
 
 root = tk.Tk()
